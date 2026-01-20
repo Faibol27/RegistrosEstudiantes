20a1428f417b9d6b2abf99f62bb50a7e4c2f9b76
Fabio cortorreal <Fabiocortorreal218@gmail.com>
2026-01-20 18:12:55 -0400

    Modelo asignatura realizado

---
+++ RegistrosEstudiantes/Models/Asignaturas.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistrosEstudiantes.Models
+{
+    public class Asignaturas
+    {
+        [Key]
+        public int AsignaturasId { get; set; }
+
+        [Required(ErrorMessage = "Este campo es requerido")]
+        public int? Codigo { get; set; }
+
+        [Required(ErrorMessage = "Este campo es requerido")]
+        public string nombre { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Este campo es requerido")]
+        public string Aula { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Este campo es requerido")]
+        [Range(1,4,ErrorMessage ="Los creditos van en este rango de numeros")]
+        public int? Creditos { get; set; }
+    }
+}
