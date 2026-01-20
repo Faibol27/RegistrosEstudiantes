2530eda8e49fdc5cd8881f77d7bbd40182c946db
Fabio cortorreal <Fabiocortorreal218@gmail.com>
2026-01-20 18:33:33 -0400

    Services inyectado correctamente

--- RegistrosEstudiantes/Program.cs
+++ RegistrosEstudiantes/Program.cs
@@ -13,6 +13,7 @@ builder.Services.AddRazorComponents()
 var ConStr = builder.Configuration.GetConnectionString("SqlConStr");
 builder.Services.AddDbContextFactory<Contexto>(o => o.UseSqlServer(ConStr));
 builder.Services.AddScoped<EstudianteServices>();
+builder.Services.AddScoped<AsignaturaServices>();
 
 var app = builder.Build();
 
